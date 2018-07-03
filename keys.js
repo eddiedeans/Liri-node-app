450247b456941c0e85bf2480d657d3c1592b52ae
Eddie Borja <eddie@Eddies-MBP.frontierlocal.net>
2018-07-02 17:32:22 -0700

    couldnt complete it

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.bTGzmol62WHpPN5GRL8Ab03sQ,
+  consumer_secret: process.env.KhBbj0DMBv9Z5GYdwllimF1gBeXcp6Y4mpDYfchLfN848fGasf,
+  access_token_key: process.env.1013076707224387590-c0oVItF6CHlZvzrLGQW04szSZavQaT,
+  access_token_secret: process.env.LiJVeeXZV65eyN6RQD7eTMRg4spQrzkkhPYOhU8r0ievB
+};
+
+exports.spotify = {
+  id: process.env.f8380d4beb854d4b8ab2e9f40c7e8267,
+  secret: process.env.473a63209055466aa6b17b586c5a9360
+};
---
+++ liri.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
